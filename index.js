2df6cb05e3b572f91a042b5dd4fac5fb7d3a225d
biancavee15 <vasquezbmarie@gmail.com>
2018-02-23 12:42:01 -0500

    updated js with function that gets new item and puts it into store

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const STORE = [
 //responsible for storing underlying data 
 //an array of objects 
 //with a checked property that indicates if it has a line through it
-<<<<<<< HEAD
 function generateItemElement(item, itemIndex, template){
     return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
@@ -24,29 +23,6 @@ function generateItemElement(item, itemIndex, template){
     </div>
   </li>`;
 }
-=======
-function generateItemElemet(item, itemIndex, template){
-    return `
-    <li class="js-item-index-element" data-item-index="${itemIndex}">
-        <span class="shopping-item js-shopping-item ${item.checked ? " shopping-item__checked" : ''}">${item.name}</span>
-        <div class="shopping-item-controls">
-            <button class="shopping-item-toggle js-item-toggle">
-                <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete js-item-delete">
-                <span class="button-label">delete</span>
-            </button>
-        </div>
-    </li >`;
-}
-
->>>>>>> 6edf0e2cbfb26a646f1fe6d86322441c7b0d65d3
-
-function generateShoppingItemString(shoppingList) {
-    console.log('Genarating shopping list element');
-    const items = shoppingList.map(item, index) =>generateItemElement(item, index));
-    retun items.join(" ");
-}
 
 function generateShoppingItemString(shoppingList){
     console.log("Generating shopping list element");
@@ -58,18 +34,27 @@ function renderShoppingList(){
     // rendering shopping list to dom
     console.log('`renderShoppingList` ran');
     const shoppingListItemString = generateShoppingItemString(STORE);
-<<<<<<< HEAD
     //insert that html into the DOM
     $('.js-shopping-list').html(shoppingListItemString);
-=======
-    /inserting HTML code in the dom
-    $('.js-shopping-list').html(shoppingListItemsString);
-
->>>>>>> 6edf0e2cbfb26a646f1fe6d86322441c7b0d65d3
+}
+function addItemToShoppingList(itemName){
+    //this function update items into the store
+    console.log(`Adding"${itemName}" to shopping list`);
+    //push new obj into store
+    STORE.push({name: itemName, checked: false});
 }
 function handleNewItemSubmit(){
     //responsible when users add new item to list
-    console.log('`handleNewItemsSubmit` ran');
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        const newItemName = $('.js-shopping-list-entry').val();
+        console.log(newItemName);
+        $('.js-shopping-list-entry').val(''); 
+        addItemToShoppingList(newItemName);
+        //making sure to add the new item into the addItemToShoppingList function
+        renderShoppingList();    
+        //call function to rerender the list with the new items
+    });
 }
 function handleItemCheckClicked(){
     //responsible for when user clicks 'check' button
